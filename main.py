d0ff9f440f73a720987266c8b4abe8a4ab156e3a
geraldo ferreira <geraldosousadev@gmail.com>
2025-01-31 15:00:41 -0300

    base project, get title

--- main.py
+++ main.py
@@ -1,3 +1,13 @@
 import requests
-from bs4 import BeautifulSoup
+from bs4 import BeautifulSoup as bs
+
+headers = { "User-Agent" : 'Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:47.0) Gecko/20100101 Firefox/47.0' }
+
+url = 'https://inglescompensadores.com.br/texto-em-ingles-com-audio-1-mike-tyson/'
+
+req = requests.get(url, headers=headers)
+soup = bs(req.content, "html.parser")
+
+titulo = soup.find('h1')['title']
+
 
